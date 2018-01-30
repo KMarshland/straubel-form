5894d863b38c5ffe3f3bd83faa010cd56167b52f
Kai Marshland <kaimarshland@gmail.com>
2018-01-29 22:34:06 -0800

    Made it send events for inputs

--- app/helpers/responses_helper.rb
+++ app/helpers/responses_helper.rb
@@ -10,6 +10,8 @@ module ResponsesHelper
 
     opts[:type] = 'textarea' if opts[:type].blank?
 
+    opts[:id] = "question-#{(0...16).map { ('a'..'z').to_a[rand(26)] }.join}"
+
     render partial: 'question', locals: opts
   end
 
