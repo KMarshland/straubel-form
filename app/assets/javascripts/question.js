a09498fb71963d3055c8b01ec9615117682bce96
Kai Marshland <kaimarshland@gmail.com>
2018-02-06 11:19:52 -0800

    Made frontend for ranking

--- app/assets/javascripts/question.js
+++ app/assets/javascripts/question.js
@@ -7,25 +7,31 @@ function initializeQuestion(opts) {
     var parent = document.getElementById(id);
     var input = parent.getElementsByTagName('input')[0] || parent.getElementsByTagName('textarea')[0];
 
-    input.addEventListener('focus', function () {
-        createEvent({
-            kind: 'questionFocus',
-            question: name
-        })
-    }, false);
+    // input.addEventListener('focus', function () {
+    //     createEvent({
+    //         kind: 'questionFocus',
+    //         question: name
+    //     })
+    // }, false);
+    //
+    // input.addEventListener('blur', function () {
+    //     if (input.value) {
+    //         createEvent({
+    //             kind: 'questionFill',
+    //             question: name
+    //         });
+    //     }
+    //
+    //
+    //     createEvent({
+    //         kind: 'questionBlur',
+    //         question: name
+    //     });
+    // }, false);
 
-    input.addEventListener('blur', function () {
-        if (input.value) {
-            createEvent({
-                kind: 'questionFill',
-                question: name
-            });
-        }
-
-
-        createEvent({
-            kind: 'questionBlur',
-            question: name
-        });
-    }, false);
+    if (opts.type === 'rank') {
+        Sortable.create(parent.getElementsByTagName('ul')[0]);
+    }
 }
+
+
